44f313396cc9255d533304ab626d98520377b897
benefit50 <d.godiac@webdelo.org>
2018-04-18 11:40:00 +0300

    Added necessary migrations

---
+++ database/migrations/2018_04_17_054655_create_tweets_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tweet_id');
+            $table->string('twitter_user_id');
+            $table->integer('retweets');
+
+            $table->integer('retweets_followers')->default(0);
+
+            $table->text('text');
+            $table->integer('status')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
